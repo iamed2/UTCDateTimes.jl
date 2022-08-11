27b3b1002fc8bdca810c407d3c224ac5c715b587
Documenter.jl <documenter@juliadocs.github.io>
2022-08-11 15:13:07 +0000

    build based on 73e2e7e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UTCDateTimes","category":"page"},{"location":"#UTCDateTimes","page":"Home","title":"UTCDateTimes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UTCDateTimes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UTCDateTimes]","category":"page"}]
+}
