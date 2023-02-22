8251efb02bbffc1b040e2dc80a6d790c07d8804f
Eric Davies <iamed2@gmail.com>
2023-02-22 17:33:43 -0600

    Rename unix2utc to unix2utcdt (matches unix2zdt)

--- src/UTCDateTimes.jl
+++ src/UTCDateTimes.jl
@@ -1,6 +1,6 @@
 module UTCDateTimes
 
-export UTCDateTime, unix2utc
+export UTCDateTime, unix2utcdt
 
 using Dates
 using TimeZones
@@ -23,12 +23,12 @@ UTCDateTime(d::Date, t::Time) = UTCDateTime(DateTime(d, t))
 UTCDateTime(zdt::ZonedDateTime) = UTCDateTime(DateTime(zdt, Dates.UTC))
 
 """
-    unix2utc(x) -> DateTime
+    unix2utcdt(x) -> DateTime
 
 Take the number of seconds since unix epoch `1970-01-01T00:00:00` and convert to the
 corresponding `UTCDateTime`.
 """
-unix2utc(x) = UTCDateTime(unix2datetime(x))
+unix2utcdt(x) = UTCDateTime(unix2datetime(x))
 
 include("dates.jl")
 include("timezones.jl")
